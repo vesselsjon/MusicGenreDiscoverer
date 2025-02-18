829ad5be22b0367fb99ee4e98f0d0bc33011d378
vesselsjon <vesselsjonathan@gmail.com>
2025-02-18 13:44:04 -0500

    Created client app and server api with default user get method

---
+++ server/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins='*')
+
+@app.route("/api/users", methods=['GET'])
+def users():
+    return jsonify(
+        {
+            "users": [
+                'jon',
+                'jacob',
+                'emi',
+                'doc',
+                'blake',
+                'ryan',
+            ]
+        }
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
